7982be51302a74aa41e23d4deb7bfdc1e63f99a6
Diego Alvirde Martinez <dalvirdem@uaemex.mx>
2020-11-12 18:23:13 -0600

    Vista index y botones de acción

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,5 @@ Route::get('/blog/{post}', 'PageController@post')->name('post');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('posts', 'Backend\PostController')->middleware('auth')->except('show');
